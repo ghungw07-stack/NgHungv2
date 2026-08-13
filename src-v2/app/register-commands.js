9ad93771054332e7140a8853f606ca46bd10cb0b
Hưng <ghungw07@gmail.com>
2026-08-14 02:56:07 +0700

    feat(v2): thêm nhóm tương tác vui chạy cục bộ

--- src-v2/app/register-commands.js
+++ src-v2/app/register-commands.js
@@ -30,6 +30,7 @@ import { registerAccountCommands } from "../modules/account/commands.js";
 import { registerAdminCommands } from "../modules/admins/commands.js";
 import { registerOperationCommands } from "../modules/operations/commands.js";
 import { registerBulkGroupCommands } from "../modules/bulk-groups/commands.js";
+import { registerFunCommands } from "../modules/fun/commands.js";
 
 export function registerRuntimeCommands(registry, dependencies) {
   const d = dependencies;
@@ -65,5 +66,6 @@ export function registerRuntimeCommands(registry, dependencies) {
   registerAdminCommands(registry, { store: d.adminStore, botId: d.botId });
   registerOperationCommands(registry, { settings: d.groupSettings, adminStore: d.adminStore, botId: d.botId });
   registerBulkGroupCommands(registry, { client: d.client, groups: d.groups });
+  registerFunCommands(registry);
   return registry;
 }
